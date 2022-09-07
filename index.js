d6109e0ca5f8d808ff1a47bb8c276e15f6696c05
Jeff <odhiambo47@gmail.com>
2022-09-07 20:23:40 +0300

    removed image slider

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//added event listeners
 const close=document.querySelector(".close"); 
 const open=document.querySelector(".ham"); 
 const menu=document.querySelector(".menu");
@@ -9,3 +10,16 @@ close.addEventListener("click",()=>{
 open.addEventListener("click",()=>{
     menu.style.visibility="visible";
 }) 
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '808339c8a2mshb4c5c628794c95dp1aebc8jsnadfa4bf4a9bf',
+		'X-RapidAPI-Host': 'google-books.p.rapidapi.com'
+	}
+};
+
+fetch('https://google-books.p.rapidapi.com/volumes?key=undefined', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
