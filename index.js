f30a86aa27abea844400df0ff3d8292d0dac8660
Jeff <odhiambo47@gmail.com>
2022-09-07 22:42:08 +0300

    correction made to the html and css files

--- index.js
+++ index.js
@@ -11,15 +11,3 @@ open.addEventListener("click",()=>{
     menu.style.visibility="visible";
 }) 
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '808339c8a2mshb4c5c628794c95dp1aebc8jsnadfa4bf4a9bf',
-		'X-RapidAPI-Host': 'google-books.p.rapidapi.com'
-	}
-};
-
-fetch('https://google-books.p.rapidapi.com/volumes?key=undefined', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
